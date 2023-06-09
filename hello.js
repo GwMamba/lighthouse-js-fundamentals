08a7d84970d95e96db8add013369fcc8daff993a
Gerald <gerald.mwang@gmail.com>
2023-06-09 04:56:14 -0600

    Improved hello function

--- hello.js
+++ hello.js
@@ -1,5 +1,6 @@
-const sayHello = function() {
-  console.log("Hello, world");
+const sayHello = function(name) {
+  console.log("Hello, " + name);
 }
-
-sayHello();
+sayHello("Paul");
+sayHello("Bene Geserit");
+sayHello("Duncan Idaho");
